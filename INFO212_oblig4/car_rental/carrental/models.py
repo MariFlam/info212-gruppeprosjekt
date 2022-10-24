3fd4e52156a42b020af252e70d971a573cae3ac4
Mari Flåm <tabasco_world@hotmail.com>
2022-10-24 13:29:54 +0200

    delted old files and added the new ones. Added the carrental app and the classes for the objects in models (car, custmer, employee)

--- INFO212_oblig4/car_rental/carrental/models.py
+++ INFO212_oblig4/car_rental/carrental/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_make = models.CharField(max_length=50)
+    car_model = models.CharField(max_length=50)
+    car_year = models.CharField(max_length = 50)
+    car_location = models.CharField (max_length = 50)
+    car_rented = models.BooleanField()
+
+    # Overrides the default to string method to print the title, author, genre and price of a book object if print()
+    # is called on an object.
+    def __str__(self):
+        car_string = f'{self.car_make} {self.car_model} {self.car_year} {self.car_location} rented: {self.car_rented}'
+        return car_string
+
+
+
+class Customer(models.Model):
+    cus_name = models.CharField(max_length=256)
+    cus_address = models.CharField(max_length=50)
+    cus_age = models.CharField(max_length = 50)
+
+    # Overrides the default to string method to print the title, author, genre and price of a book object if print()
+    # is called on an object.
+    def __str__(self):
+        cus_string = f'{self.cus_name} {self.cus_age} {self.cus_address}'
+        return cus_string
+
+
+
+class Customer(models.Model):
+    emp_name = models.CharField(max_length=256)
+    emp_address = models.CharField(max_length=50)
+    emp_branch = models.CharField(max_length = 50)
+
+    # Overrides the default to string method to print the title, author, genre and price of a book object if print()
+    # is called on an object.
+    def __str__(self):
+        emp_string = f'{self.emp_name} {self.emp_address} {self.emp_branch} '
+        return emp_string
+
--- INFO212_oblig4/car_rental/carrental/views.py
+++ INFO212_oblig4/car_rental/carrental/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from carrental.models import Car
 
-# Create your views here.
+def cars(request):
+    car = Car.objects.all().order_by('title')
+    context = {
+        'cars': car
+    }
+    return render(request, 'car_list.html', context)
+
+
+"""# Create your views here.
+create, add, delete
+crud bruke dette imetodene for å endre på de lagrede objektene og legge til nye
+
+
+get, post and put changes through the api"""
