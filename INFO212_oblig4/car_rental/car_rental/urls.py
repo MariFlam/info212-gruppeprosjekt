228b1d7ae59fa06a355a00e274d8d6c704e586be
Angelica Skiple <angelicaskiple@Angelicas-Air.home>
2022-11-02 00:57:22 +0100

    Added the paths to the new methods, in urls.py

--- INFO212_oblig4/car_rental/car_rental/urls.py
+++ INFO212_oblig4/car_rental/car_rental/urls.py
@@ -29,7 +29,10 @@ urlpatterns = [
     path('update_employee/<int:id>', view.update_employee, name = 'Change existing employee'),
     path('delete_employee/<int:id>', view.delete_employee, name = 'Delete employee'),
     #order, Rent, Delete Booking, Return car paths
-    path('order_car/<int:id>/<int:vin>', view.order_car, name= 'Order car'),
+    path('order_car/<int:id>/<int:vin>', view.order_car, name = 'Order car'),
+    path('cancel_order_car/<int:id>/<int:vin>', view.cancel_order_car, name = 'Cancel order car'),
+    path('rent_car/<int:id>/<int:vin>', view.rent_car, name = 'Rent car'),
+    path('return_car/<int:id>/<int:vin>', view.return_car, name = 'Return car')
 
     ]
 
